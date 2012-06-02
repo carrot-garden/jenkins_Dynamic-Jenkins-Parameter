b0e7f6ee8fc953c3efdcf2db67b09e0e1cefa62b
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2012-06-02 16:58:50 -0500

    a

--- src/main/java/com/tekante/jenkins/DynamicParameter.java
+++ src/main/java/com/tekante/jenkins/DynamicParameter.java
@@ -1,126 +1,155 @@
 package com.tekante.jenkins;
 
 import hudson.Extension;
-
-import hudson.model.Hudson;
 import hudson.model.ParameterValue;
+import hudson.model.Hudson;
+import hudson.model.Job;
 import hudson.model.ParameterDefinition;
 import hudson.model.ParametersDefinitionProperty;
-import hudson.model.StringParameterValue;
 import hudson.util.ListBoxModel;
-import hudson.model.Job;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.logging.Logger;
 
 import net.sf.json.JSONObject;
 
-import org.apache.log4j.lf5.LogLevel;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
-import java.io.UnsupportedEncodingException;
-
 /**
- * String based parameter that supports picking the string from two lists of values
- * presented at build time generated from data specified in job configuration
- * for this parameter and organized such that the command for the second list sees
- * the value from the first list and can change allowed values dynamically
- *
+ * String based parameter that supports picking the string from two lists of
+ * values presented at build time generated from data specified in job
+ * configuration for this parameter and organized such that the command for the
+ * second list sees the value from the first list and can change allowed values
+ * dynamically
+ * 
  * @author Chris Johnson
  * @see {@link ParameterDefinition}
  */
 
 public class DynamicParameter extends ParameterDefinition {
-  private static final Logger LOG = Logger.getLogger(DynamicParameter.class.getName());
-  static final long serialVersionUID = 4;
-  public String value = "";
-  public String dynamicValue = "";
-  public String valueOptions;
-  public String dynamicValueOptions;
-  public String secondName;
 
-  @DataBoundConstructor
-  public DynamicParameter(String name, String description, String valueOptions, String dynamicValueOptions, String secondName) {
-    super(name, description);
-    this.secondName = secondName;
-    this.valueOptions = valueOptions;
-    this.dynamicValueOptions = dynamicValueOptions;
-  }
+	private static final Logger LOG = Logger.getLogger(DynamicParameter.class
+			.getName());
 
-  @Extension
-  public static final class DescriptorImpl extends ParameterDescriptor {
-    @Override
-    public String getDisplayName() {
-      return "Dynamic Parameter";
-    }
+	static final long serialVersionUID = 4;
+	public String value = "";
+	public String dynamicValue = "";
+	public String valueOptions;
+	public String dynamicValueOptions;
+	public String secondName;
 
-    private DynamicParameter getDynamicParameter(String param) {
-      String containsJobName = getCurrentDescriptorByNameUrl();
-      String jobName = null;
-      try {
-        jobName = java.net.URLDecoder.decode(containsJobName.substring(containsJobName.lastIndexOf("/") + 1), "UTF-8");
-      } catch (UnsupportedEncodingException e) {
-        LOG.warning("Could not find parameter definition instance for parameter " + param + " due to encoding error in job name: " + e.getMessage());
-        return null;
-      }
+	@DataBoundConstructor
+	public DynamicParameter(final String name, final String description,
+			final String valueOptions, final String dynamicValueOptions,
+			final String secondName) {
+		super(name, description);
+		this.secondName = secondName;
+		this.valueOptions = valueOptions;
+		this.dynamicValueOptions = dynamicValueOptions;
+	}
 
-      Job<?, ?> j = Hudson.getInstance().getItemByFullName(jobName, hudson.model.Job.class);
-      if (j != null) {
-        ParametersDefinitionProperty pdp = j.getProperty(hudson.model.ParametersDefinitionProperty.class);
-        List<ParameterDefinition> pds = pdp.getParameterDefinitions();
-        for (ParameterDefinition pd : pds) {
-          if (this.isInstance(pd) && ((DynamicParameter) pd).getName().equalsIgnoreCase(param)) {
-            return (DynamicParameter) pd;
-          }
-        }
-      }
-      LOG.warning("Could not find parameter definition instance for parameter " + param);
-      return null;
-    }
+	@Extension
+	public static final class DescriptorImpl extends ParameterDescriptor {
 
-    public ListBoxModel doFillValueItems(@QueryParameter String name) {
-      LOG.finer("Called with param: " + name);
-      ListBoxModel m = new ListBoxModel();
+		@Override
+		public String getDisplayName() {
+			return "Dynamic Parameter";
+		}
 
-      DynamicParameter dp = this.getDynamicParameter(name);
-      if (dp != null) {
-        for (String s : dp.valueOptions.split("\\r?\\n")) {
-          m.add(s);
-        }
-      }
-      return m;
-    }
+		private DynamicParameter getDynamicParameter(final String param) {
+			final String containsJobName = getCurrentDescriptorByNameUrl();
+			String jobName = null;
+			try {
+				jobName = java.net.URLDecoder.decode(containsJobName
+						.substring(containsJobName.lastIndexOf("/") + 1),
+						"UTF-8");
+			} catch (final UnsupportedEncodingException e) {
+				LOG.warning("Could not find parameter definition instance for parameter "
+						+ param
+						+ " due to encoding error in job name: "
+						+ e.getMessage());
+				return null;
+			}
 
-    public ListBoxModel doFillDynamicValueItems(@QueryParameter String name, @QueryParameter String value) {
-      ListBoxModel m = new ListBoxModel();
+			final Job<?, ?> j = Hudson.getInstance().getItemByFullName(jobName,
+					hudson.model.Job.class);
+			if (j != null) {
+				final ParametersDefinitionProperty pdp = j
+						.getProperty(hudson.model.ParametersDefinitionProperty.class);
+				final List<ParameterDefinition> pds = pdp
+						.getParameterDefinitions();
+				for (final ParameterDefinition pd : pds) {
+					if (this.isInstance(pd)
+							&& ((DynamicParameter) pd).getName()
+									.equalsIgnoreCase(param)) {
+						return (DynamicParameter) pd;
+					}
+				}
+			}
+			LOG.warning("Could not find parameter definition instance for parameter "
+					+ param);
+			return null;
+		}
 
-      DynamicParameter dp = this.getDynamicParameter(name);
-      if (dp != null) {
-        for (String s : dp.dynamicValueOptions.split("\\r?\\n")) {
-          if (s.indexOf(value) == 0) {
-            String[] str = s.split(":");
-            m.add(str[1]);
-          }
-        }
-      }
-      return m;
-    }
-  }
+		public ListBoxModel doFillValueItems(@QueryParameter final String name) {
+			LOG.finer("Called with param: " + name);
+			final ListBoxModel m = new ListBoxModel();
 
-  @Override
-  public ParameterValue createValue(StaplerRequest req, JSONObject jo) {
-    DynamicParameterValue value = req.bindJSON(DynamicParameterValue.class, jo);
-    return value;
-  }
+			final DynamicParameter dp = this.getDynamicParameter(name);
+			if (dp != null) {
+				for (final String s : dp.valueOptions.split("\\r?\\n")) {
+					m.add(s);
+				}
+			}
+			return m;
+		}
 
-  @Override
-  public ParameterValue createValue(StaplerRequest req) {
-    String[] value = req.getParameterValues(getName());
-    String[] dynamicValue = req.getParameterValues(this.secondName);
-    LOG.warning(getName() + ": " + value[0] + "\n");
-    LOG.warning(this.secondName + ": " + dynamicValue[0] + "\n");
-    return new DynamicParameterValue(getName(), value[0], this.secondName, dynamicValue[0]);
-  }
+		public ListBoxModel doFillDynamicValueItems(
+				@QueryParameter final String name,
+				@QueryParameter final String value) {
+			final ListBoxModel m = new ListBoxModel();
+
+			final DynamicParameter dp = this.getDynamicParameter(name);
+			if (dp != null) {
+				for (final String s : dp.dynamicValueOptions.split("\\r?\\n")) {
+					if (s.indexOf(value) == 0) {
+						final String[] str = s.split(":");
+						m.add(str[1]);
+					}
+				}
+			}
+			return m;
+		}
+	}
+
+	@Override
+	public ParameterValue createValue(final StaplerRequest req,
+			final JSONObject jo) {
+
+		final DynamicParameterValue value = req.bindJSON(
+				DynamicParameterValue.class, jo);
+
+		return value;
+
+	}
+
+	@Override
+	public ParameterValue createValue(final StaplerRequest req) {
+
+		final String[] value = req.getParameterValues(getName());
+
+		final String[] dynamicValue = req.getParameterValues(this.secondName);
+
+		LOG.warning(getName() + ": " + value[0] + "\n");
+
+		LOG.warning(this.secondName + ": " + dynamicValue[0] + "\n");
+
+		return new DynamicParameterValue(getName(), value[0], this.secondName,
+				dynamicValue[0]);
+
+	}
+
 }
--- src/main/java/com/tekante/jenkins/DynamicParameterValue.java
+++ src/main/java/com/tekante/jenkins/DynamicParameterValue.java
@@ -1,68 +1,82 @@
 package com.tekante.jenkins;
 
-import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.export.Exported;
-
 import hudson.EnvVars;
 import hudson.model.AbstractBuild;
 import hudson.model.StringParameterValue;
 import hudson.util.VariableResolver;
 
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.export.Exported;
+
 public class DynamicParameterValue extends StringParameterValue {
-  private static final long serialVersionUID = 1L;
-  @Exported(visibility=4)
-  public final String dynamicName;
-  public final String dynamicValue;
 
-  @DataBoundConstructor
-  public DynamicParameterValue(String name, String value, String secondName, String dynamicValue) {
-    this(name, value, secondName, dynamicValue, null);
-  }
+	private static final long serialVersionUID = 1L;
 
-  public DynamicParameterValue(String name, String value, String secondName, String dynamicValue, String description) {
-    super(name, value, description);
-    this.dynamicName = secondName;
-    this.dynamicValue = dynamicValue;
-  }
+	@Exported(visibility = 4)
+	public final String dynamicName;
+	public final String dynamicValue;
 
-  /**
-   * Exposes the name/value as an environment variable.
-   */
-  @Override
-  public void buildEnvVars(AbstractBuild<?,?> build, EnvVars env) {
-    env.put(name, value);
-    env.put(dynamicName, dynamicValue);
-  }
+	@DataBoundConstructor
+	public DynamicParameterValue(final String name, final String value,
+			final String secondName, final String dynamicValue) {
+		this(name, value, secondName, dynamicValue, null);
+	}
 
-  @Override
-  public VariableResolver<String> createVariableResolver(AbstractBuild<?, ?> build) {
-    return new VariableResolver<String>() {
-      public String resolve(String name) {
-        return DynamicParameterValue.this.name.equals(name) ? value : (DynamicParameterValue.this.dynamicName.equals(name) ? dynamicValue : null);
-      }
-    };
-  }
+	public DynamicParameterValue(final String name, final String value,
+			final String secondName, final String dynamicValue,
+			final String description) {
+		super(name, value, description);
+		this.dynamicName = secondName;
+		this.dynamicValue = dynamicValue;
+	}
 
-  @Override
-  public int hashCode() {
-    return 29 * (value.hashCode() + dynamicValue.hashCode() + name.hashCode() + dynamicName.hashCode());
-  }
+	/**
+	 * Exposes the name/value as an environment variable.
+	 */
+	@Override
+	public void buildEnvVars(final AbstractBuild<?, ?> build, final EnvVars env) {
+		env.put(name, value);
+		env.put(dynamicName, dynamicValue);
+	}
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
+	@Override
+	public VariableResolver<String> createVariableResolver(
+			final AbstractBuild<?, ?> build) {
+		return new VariableResolver<String>() {
+			public String resolve(final String name) {
+				return DynamicParameterValue.this.name.equals(name) ? value
+						: (DynamicParameterValue.this.dynamicName.equals(name) ? dynamicValue
+								: null);
+			}
+		};
+	}
 
-    if (obj instanceof DynamicParameterValue) {
-      DynamicParameterValue other = (DynamicParameterValue)obj;
-      return name.equals(other.getName()) && dynamicName.equals(other.dynamicName) && value.equals(other.value) && dynamicValue.equals(other.dynamicValue);
-    }
+	@Override
+	public int hashCode() {
+		return 29 * (value.hashCode() + dynamicValue.hashCode()
+				+ name.hashCode() + dynamicName.hashCode());
+	}
 
-    return false;
-  }
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
 
-  @Override
-  public String toString() {
-    return "(DynamicParameterValue) " + getName() + "='" + value + "', " + this.dynamicName + "='" + dynamicValue + "'";
-  }
+		if (obj instanceof DynamicParameterValue) {
+			final DynamicParameterValue other = (DynamicParameterValue) obj;
+			return name.equals(other.getName())
+					&& dynamicName.equals(other.dynamicName)
+					&& value.equals(other.value)
+					&& dynamicValue.equals(other.dynamicValue);
+		}
+
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "(DynamicParameterValue) " + getName() + "='" + value + "', "
+				+ this.dynamicName + "='" + dynamicValue + "'";
+	}
+
 }
